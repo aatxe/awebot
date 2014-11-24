7a10abfd98428c103dbea153e613814337436439
Aaron Weiss <aaronweiss74@gmail.com>
2014-11-24 12:49:13 -0500

    Plugins are now only reloaded on change.

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ extern crate irc;
 
 use std::collections::HashMap;
 use std::dynamic_lib::DynamicLibrary;
+use std::fmt::{Error, Formatter, Show};
 use std::io::{BufferedStream, IoResult};
-use std::io::fs::walk_dir;
+use std::io::fs::{PathExtensions, walk_dir};
 use irc::conn::NetStream;
 use irc::data::{Config, Message};
 use irc::server::{IrcServer, Server};
@@ -26,23 +27,53 @@ fn main() {
     let irc_server = IrcServer::from_config(config).unwrap();
     let server = Wrapper::new(&irc_server);
     server.identify().unwrap();
+    let mut cache = HashMap::new();
     for message in server.iter() {
         print!("{}", message.into_string());
-        process_message_dynamic(&server, message).unwrap();
+        process_message_dynamic(&server, message, &mut cache).unwrap();
     }
 }
 
-fn process_message_dynamic<'a>(server: &'a Wrapper<'a, BufferedStream<NetStream>>, 
-                               message: Message) -> IoResult<()> {
+struct Function<'a> { 
+    lib: DynamicLibrary,
+    pub process: Option<fn(&'a Wrapper<'a, BufferedStream<NetStream>>, Message) -> IoResult<()>>,
+    pub modified: u64,
+}
+
+impl<'a> Function<'a> {
+    fn eval(&mut self) {
+        self.process = Some(unsafe {
+            std::mem::transmute(self.lib.symbol::<u8>("process").unwrap())
+        });
+    }
+}
+
+impl<'a> Show for Function<'a> {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        try!("fn (server, message) -> IoResult<()> : ".fmt(fmt));
+        self.modified.fmt(fmt)
+    }
+}
+
+fn process_message_dynamic<'a>(server: &'a Wrapper<'a, BufferedStream<NetStream>>,
+                               message: Message, cache: &mut HashMap<String, Function<'a>>) 
+-> IoResult<()> {
     for path in walk_dir(&Path::new("plugins/")).unwrap() {
         if path.extension().is_none() || path.extension().unwrap() != b"dylib" &&
            path.extension().unwrap() != b"so" { continue }
-        let lib = DynamicLibrary::open(Some(path.as_str().unwrap())).unwrap();            
-        let process: fn(&'a Wrapper<'a, BufferedStream<NetStream>>, Message) -> IoResult<()> = 
-            unsafe {
-            std::mem::transmute(lib.symbol::<u8>("process").unwrap())
-        };
-        try!(process(server, message.clone()));
+        let modified = path.stat().unwrap().modified;
+        let key = path.as_str().unwrap().into_string();
+        if !cache.contains_key(&key) || cache[key].modified != modified {
+            let lib = DynamicLibrary::open(Some(path.as_str().unwrap())).unwrap();   
+            let mut func = Function { 
+                lib: lib,
+                process: None,
+                modified: modified,
+            };
+            func.eval();
+            cache.insert(key.clone(), func);
+        }
+        try!((cache[key].process.unwrap())(server, message.clone()));    
     }
     Ok(())
 }
