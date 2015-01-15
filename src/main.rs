49390b5504a2f73e3672da73164e848ddf632542
Aaron Weiss <aaronweiss74@gmail.com>
2015-01-15 13:19:08 -0500

    Updated dependencies.

--- src/main.rs
+++ src/main.rs
@@ -57,7 +57,6 @@ fn process_message_dynamic<'a>(server: &'a NetWrapper<'a>, message: Message,
         let modified = path.stat().unwrap().modified;
         let key = path.as_str().unwrap().to_owned();
         if !cache.contains_key(&key) || cache[key].modified != modified {
-            //unsafe { std::mem::forget(cache.remove(&key)) } // leak to prevent segfaults
             cache.remove(&key);
             let lib = DynamicLibrary::open(Some(&path)).unwrap();   
             let func = Function { 
