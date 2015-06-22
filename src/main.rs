426755868b0b43847b3d0e66a18d97dda3acd852
Aaron Weiss <aaronweiss74@gmail.com>
2015-06-22 12:02:17 -0400

    Removed trailing whitespace.

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ fn main() {
 
 type NetServer<'a> = ServerExt<'a, BufReader<NetStream>, BufWriter<NetStream>>;
 
-struct Function<'a> { 
+struct Function<'a> {
     _lib: DynamicLibrary,
     pub process: fn(&'a NetServer<'a>, Message) -> Result<()>,
     pub modified: u64,
@@ -65,15 +65,15 @@ fn process_message_dynamic<'a>(server: &'a NetServer<'a>, message: Message,
     let valid: [&OsStr; 3] = ["dylib".as_ref(), "so".as_ref(), "dll".as_ref()];
     for path in walk_dir(&Path::new("plugins/")).unwrap() {
         let path = try!(path).path();
-        if path.extension().is_none() || !valid.contains(&path.extension().unwrap()) { 
-            continue 
+        if path.extension().is_none() || !valid.contains(&path.extension().unwrap()) {
+            continue
         }
         let modified = try!(modified(&path));
         let key = path.clone().into_os_string().into_string().unwrap();
         if !cache.contains_key(&key) || cache[&key].modified != modified {
             cache.remove(&key);
-            let lib = DynamicLibrary::open(Some(&path)).unwrap();   
-            let func = Function { 
+            let lib = DynamicLibrary::open(Some(&path)).unwrap();
+            let func = Function {
                 process: unsafe {
                     std::mem::transmute(lib.symbol::<u8>("process").unwrap())
                 },
@@ -82,7 +82,7 @@ fn process_message_dynamic<'a>(server: &'a NetServer<'a>, message: Message,
             };
             cache.insert(key.clone(), func);
         }
-        try!((cache[&key].process)(server, message.clone()));    
+        try!((cache[&key].process)(server, message.clone()));
     }
     Ok(())
 }
