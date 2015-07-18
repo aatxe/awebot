65f1fe9a968a99ff545758d891dc272e0fd1490b
Aaron Weiss <aaronweiss74@gmail.com>
2015-07-17 20:50:28 -0400

    Added multi-server support for awebot.

--- src/main.rs
+++ src/main.rs
@@ -10,30 +10,43 @@ use std::io::{BufReader, BufWriter, Result};
 use std::io::prelude::*;
 use std::path::Path;
 use std::result::Result as StdResult;
+use std::thread::spawn;
 #[cfg(windows)] use std::os::windows::fs::MetadataExt;
 #[cfg(unix)] use std::os::unix::fs::MetadataExt;
 use irc::client::conn::NetStream;
 use irc::client::prelude::*;
 
 fn main() {
-    let server = IrcServer::new("config.json").unwrap();
-    loop {
-        server.identify().unwrap();
-        let mut cache = HashMap::new();
-        for message in server.iter() {
-            match message {
-                Ok(message) => {
-                    print!("{}", message.into_string());
-                    process_message_dynamic(&server, message, &mut cache).unwrap();
-                },
-                Err(e) => {
-                    println!("Reconnecting because {}", e);
-                    break
+    let guards: Vec<_> = walk_dir(".").unwrap().flat_map(|p| {
+        let path = p.unwrap().path();
+        println!("path: {:?}", path);
+        path.clone().extension().map(|ext| match ext.to_str() {
+            Some("json") => Some(Config::load(path).unwrap()),
+            _ => None,
+        }).into_iter().filter(|c| c.is_some()).map(|c| c.unwrap())
+    }).map(|config| {
+        spawn(|| {
+            let server = IrcServer::from_config(config).unwrap();
+            loop {
+                server.identify().unwrap();
+                let mut cache = HashMap::new();
+                for message in server.iter() {
+                    match message {
+                        Ok(message) => {
+                            print!("{}", message.into_string());
+                            process_message_dynamic(&server, message, &mut cache).unwrap();
+                        },
+                        Err(e) => {
+                            println!("Reconnecting because {}", e);
+                            break
+                        }
+                    }
                 }
+                server.reconnect().unwrap();
             }
-        }
-        server.reconnect().unwrap();
-    }
+        })
+    }).collect();
+    guards.into_iter().map(|h| h.join().unwrap()).count();
 }
 
 type NetServer<'a> = ServerExt<'a, BufReader<NetStream>, BufWriter<NetStream>>;
@@ -63,7 +76,7 @@ fn modified(path: &Path) -> Result<u64> {
 fn process_message_dynamic<'a>(server: &'a NetServer<'a>, message: Message,
                                cache: &mut HashMap<String, Function<'a>>) -> Result<()> {
     let valid: [&OsStr; 3] = ["dylib".as_ref(), "so".as_ref(), "dll".as_ref()];
-    for path in walk_dir(&Path::new("plugins/")).unwrap() {
+    for path in walk_dir("plugins/").unwrap() {
         let path = try!(path).path();
         if path.extension().is_none() || !valid.contains(&path.extension().unwrap()) {
             continue
