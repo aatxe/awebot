9005745bc7b1003bb77ebe67dc204d4631c53e29
Aaron Weiss <awe@pdgn.co>
2018-02-18 23:19:51 +0100

    Setup an IRC connection in main_impl.

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,43 @@
+use irc::client::prelude::*;
+
 use config::Config;
 use error::*;
 
 pub fn main_impl() -> Result<()> {
     let config = Config::load("merveille.toml")?;
+
+    let mut reactor = IrcReactor::new()?;
+    let client = reactor.prepare_client_and_connect(&config.into())?;
+    client.identify()?;
+
+    reactor.register_client_with_handler(client, |client, message| {
+        trace!("{}", message.to_string().trimmed());
+        if let Command::PRIVMSG(ref target, ref msg) = message.command {
+            if msg == "!quit" {
+                client.send_quit(format!(
+                    "Quitting: command from {}", message.source_nickname().unwrap_or(target)
+                ))?;
+            }
+        }
+        Ok(())
+    });
+
+    reactor.run()?;
     Ok(())
+}
+
+trait StringTrim {
+    fn trimmed(self) -> Self;
+}
+
+impl StringTrim for String {
+    fn trimmed(mut self) -> Self {
+        if self.ends_with('\n') {
+            self.pop();
+        }
+        if self.ends_with('\r') {
+            self.pop();
+        }
+        self
+    }
 }
